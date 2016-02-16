c868d8a35a53953fa724c8771711f4752a0e6020
daniagudelo <daniagudelo@gmail.com>
2016-02-16 21:20:06 +0000

    Added edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
   
   resources :articles
 
